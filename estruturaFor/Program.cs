85a8226ab53c450ed66542393665994edaa51509
Fernanda Sapateiro <feernaandaes@gmail.com>
2020-04-25 19:07:23 -0300

    Teste estrutura for

---
+++ estruturaFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace estruturaFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros inteiros vc vai digitar: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= num; i++) {
+                Console.Write("Valor #"+i+": ");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.WriteLine("Total: "+soma);
+        }
+    }
+}
